5217eac865f1c9a8be29d9baa4468acb0ede8957
Luiserebii <luis@serebii.io>
2020-01-13 18:23:37 -0500

    Setting up basic vector interface

--- src/vector.c
+++ src/vector.c
@@ -1,1 +1,13 @@
 #include "vector.h"
+
+vector vector_init() {
+    return { NULL, NULL, NULL };
+}
+
+void vector_push_back(vector* v) {
+    
+}
+
+size_t vector_size(vector* v) {
+    
+}
--- src/vector.h
+++ src/vector.h
@@ -1,6 +1,16 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <stdlib.h>
 
+typedef struct {
+    int* head;
+    int* avail;
+    int* tail;
+} vector;
+
+vector vector_init();
+void vector_push_back(vector* v);
+size_t vector_size(vector* v);
 
 #endif
