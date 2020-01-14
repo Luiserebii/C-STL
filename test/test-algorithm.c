8871ed40b11cf27cbd36578868d8c41c78dbd2df
Luiserebii <luis@serebii.io>
2020-01-14 18:40:01 -0500

    Added additional test for array of different type (char). The only thing is the utility function is a little repetitive; should we use a macro?

--- test/test-algorithm.c
+++ test/test-algorithm.c
@@ -3,8 +3,10 @@
 #include "./lib/unity.h"
 #include "../src/algorithm.h"
 
-void test_algorithm_h();
-int arrIsEqual(int* a, int* b, int n);
+void test_algorithm_copy_int();
+void test_algorithm_copy_char();
+int arrIntIsEqual(int* a, int* b, int n);
+int arrCharIsEqual(char* a, char* b, int n);
 
 void setUp() { }
 void tearDown() { }
@@ -12,27 +14,48 @@ void tearDown() { }
 int main() {
 
     UNITY_BEGIN();
-    RUN_TEST(test_algorithm_h);
+    RUN_TEST(test_algorithm_copy_int);
+    RUN_TEST(test_algorithm_copy_char);
     return UNITY_END();
 
 }
 
-void test_algorithm_h() {
-    
-    //Testing algorithm.h
-    int testArr[] = { 1, 2, 3 };
-    const int sz = sizeof testArr / sizeof(int);
-    int meme[sz];
+void test_algorithm_copy_int() {
+    //Setting up basic array to copy over
+    int a1[] = { 1, 2, 3 };
+    const int sz = sizeof a1 / sizeof(int);
+    int a2[sz];
 
-    //Attempt copying of testArr into meme
-    TEST_ASSERT_FALSE(arrIsEqual(testArr, meme, sz));
-    algorithm_copy(int*, testArr, testArr + sz, meme);
+    //Attempt copying of a1 into a2
+    TEST_ASSERT_FALSE(arrIntIsEqual(a1, a2, sz));
+    algorithm_copy(int*, a1, a1 + sz, a2);
 
-    //Assert copied value
-    TEST_ASSERT_EQUAL_INT_ARRAY(testArr, meme, sz);
+    //Assert copied values
+    TEST_ASSERT_EQUAL_INT_ARRAY(a1, a2, sz);
 }
 
-int arrIsEqual(int* a, int* b, int n) {
+void test_algorithm_copy_char() {
+    //Setting up basic array to copy over
+    char a1[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+    const int sz = sizeof a1 / sizeof(char);
+    char a2[sz];
+
+    //Attempt copying of a1 into a2
+    TEST_ASSERT_FALSE(arrCharIsEqual(a1, a2, sz));
+    algorithm_copy(char*, a1, a1 + sz, a2);
+
+    //Assert copied values
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(a1, a2, sz);
+}
+
+int arrIntIsEqual(int* a, int* b, int n) {
+    while(n--) {
+        if(*a != *b) return 0;
+    }
+    return 1;
+}
+
+int arrCharIsEqual(char* a, char* b, int n) {
     while(n--) {
         if(*a != *b) return 0;
     }
