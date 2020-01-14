87ac6ad4820458c4995da52b1d5df781d1f7b99b
Luiserebii <luis@serebii.io>
2020-01-13 19:10:58 -0500

    Implementation of free and grow functions; I think this is a good place to refactor for algorithms header

--- src/vector.c
+++ src/vector.c
@@ -21,6 +21,31 @@ void vector_push_back(vector* v, int e) {
     }
 }
 
+void vector_grow(vector* v) {
+    //Determine new size
+    size_t old_size = vector_size(v);
+    size_t n_size = v->head ? 1 : old_size * 2;
+
+    //Allocate new
+    int* n_head = (int*) malloc(sizeof(int) * n_size);
+
+    //Copy
+    for(int* i = v->head, * n = n_head; i != v->avail;) {
+        *n++ = *i++;
+    }
+
+    //Destroy
+    free(v->head);
+    v->head = n_head;
+    v->avail = n_head + old_size;
+    v->tail = n_head + n_size;
+}
+
 size_t vector_size(vector* v) {
     return v->avail - v->head;
+}
+
+void vector_free(vector* v) {
+    free(v->head);
+    v->head = v->avail = v->tail = NULL;
 }
--- src/vector.h
+++ src/vector.h
@@ -13,5 +13,8 @@ vector vector_init();
 void vector_init_size(vector* v, size_t s);
 void vector_push_back(vector* v, int e);
 size_t vector_size(vector* v);
+void vector_free(vector* v);
+
+static void vector_grow(vector* v);
 
 #endif
