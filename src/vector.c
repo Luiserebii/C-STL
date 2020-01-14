88bc41410cbd2697a9a5a3ebc861c3e329402ade
Luiserebii <luis@serebii.io>
2020-01-14 00:41:33 -0500

    Continued mass-attempt to convert into macro

--- src/vector.c
+++ src/vector.c
@@ -4,50 +4,53 @@
 #include "vector.h"
 #include "algorithm.h"
 
-void vector_init(vector* v) {
-    *v = (vector) { NULL, NULL, NULL };
-}
+declare_vector(int)
 
-void vector_init_size(vector* v, size_t s) {
-    v->head = v->avail = (VECTOR_TYPE*) malloc(sizeof(VECTOR_TYPE) * s);
-    v->tail = v->head + s;
-}
-
-void vector_push_back(vector* v, VECTOR_TYPE e) {
-    if(v->avail == v->tail) {
-        vector_grow(v);
-    }
-    //Append
-    *(v->avail++) = e;
-}
-
-void vector_grow(vector* v) {
-    //Determine new size
-    size_t old_size = vector_size(v);
-    size_t n_size = v->head ? old_size * 2 : 1;
-
-    //Allocate new
-    VECTOR_TYPE* n_head = (VECTOR_TYPE*) malloc(sizeof(VECTOR_TYPE) * n_size);
-
-    //Copy
-    algorithm_copy(VECTOR_TYPE*, v->head, v->avail, n_head);
-
-    //Destroy
-    free(v->head);
-    v->head = n_head;
-    v->avail = n_head + old_size;
-    v->tail = n_head + n_size;
-}
-
-size_t vector_size(vector* v) {
-    return v->avail - v->head;
-}
-
-size_t vector_capacity(vector* v) {
-    return v->tail - v->head;
-}
-
-void vector_free(vector* v) {
-    free(v->head);
-    v->head = v->avail = v->tail = NULL;
-}
+#define define_vector(vector_type) \
+void vector_init_##vector_type(vector_##vector_type* v) { \
+    *v = (vector_##vector_type) { NULL, NULL, NULL }; \
+} \
+\
+void vector_init_size_##vector_type(vector_##vector_type* v, size_t s) { \
+    v->head = v->avail = (vector_type*) malloc(sizeof(vector_type) * s); \
+    v->tail = v->head + s; \
+} \
+\
+void vector_push_back_##vector_type(vector_##vector_type* v, vector_type e) { \
+    if(v->avail == v->tail) { \
+        vector_grow_##vector_type(v); \
+    } \
+    //Append \
+    *(v->avail++) = e; \
+} \
+\
+void vector_grow_##vector_type(vector_##vector_type* v) { \
+    //Determine new size \
+    size_t old_size = vector_size_##vector_type(v); \
+    size_t n_size = v->head ? old_size * 2 : 1; \
+\
+    //Allocate new \
+    vector_type* n_head = (vector_type*) malloc(sizeof(vector_type) * n_size); \
+\
+    //Copy \
+    algorithm_copy(vector_type*, v->head, v->avail, n_head); \
+\
+    //Destroy \
+    free(v->head); \
+    v->head = n_head; \
+    v->avail = n_head + old_size; \
+    v->tail = n_head + n_size; \
+} \
+\
+size_t vector_size_##vector_type(vector_##vector_type* v) { \
+    return v->avail - v->head; \
+} \
+\
+size_t vector_capacity_##vector_type(vector_##vector_type* v) { \
+    return v->tail - v->head; \
+} \
+\
+void vector_free_##vector_type(vector_##vector_type* v) { \
+    free(v->head); \
+    v->head = v->avail = v->tail = NULL; \
+} \
