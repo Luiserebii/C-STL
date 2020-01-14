7dc0ab265fbe2b867f82fd667238d2dd7f3eb292
Luiserebii <luis@serebii.io>
2020-01-13 23:44:41 -0500

    Committing this demonic attempt at making the STL version of copy, oh my lord

--- src/algorithm.h
+++ src/algorithm.h
@@ -9,12 +9,15 @@
  *
  * Ex: algorithm_copy(int*, months, months + 12, words)
  */
+/*
 #define algorithm_copy(type, begin, end, dest) \
     { \
         type alg_copy_it = begin; \
         for(type alg_copy_dest = dest; alg_copy_it != end; *alg_copy_dest++ = *alg_copy_it++) \
             ;   \
     } \
+*/
+#define algorithm_copy(type, begin, end, dest) type alg_copy_it = begin, for(type alg_copy_dest = dest; alg_copy_it != end; *alg_copy_dest++ = *alg_copy_it++), dest
 
 
 #endif
