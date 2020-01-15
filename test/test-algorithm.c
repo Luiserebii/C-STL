47ae72d6ba9cd4d07b1ae6ddb1ac9805e06f0d59
Luiserebii <luis@serebii.io>
2020-01-14 19:18:26 -0500

    Added struct test case, is passing! I am noticing that this test framework does actually have a memory compare natively, I may try this

--- test/test-algorithm.c
+++ test/test-algorithm.c
@@ -4,8 +4,14 @@
 #include "./lib/unity.h"
 #include "../src/algorithm.h"
 
+struct point {
+    int x;
+    int y;
+};
+
 void test_algorithm_copy_int();
 void test_algorithm_copy_char();
+void test_algorithm_copy_struct();
 
 void setUp() { }
 void tearDown() { }
@@ -15,6 +21,7 @@ int main() {
     UNITY_BEGIN();
     RUN_TEST(test_algorithm_copy_int);
     RUN_TEST(test_algorithm_copy_char);
+    RUN_TEST(test_algorithm_copy_struct);
     return UNITY_END();
 
 }
@@ -27,13 +34,10 @@ void test_algorithm_copy_int() {
 
     //Attempt copying of a1 into a2
     TEST_ASSERT(memcmp(a1, a2, sizeof a1) != 0);
-    printf("MEMCMP: %d", memcmp(a1, a2, sizeof a1));
     algorithm_copy(int*, a1, a1 + sz, a2);
 
     //Assert copied values
     TEST_ASSERT_EQUAL_INT_ARRAY(a1, a2, sz);
-    TEST_ASSERT(memcmp(a1, a2, sizeof a1) == 0);
-    printf("MEMCMP: %d", memcmp(a1, a2, sizeof a1));
 }
 
 void test_algorithm_copy_char() {
@@ -44,11 +48,22 @@ void test_algorithm_copy_char() {
 
     //Attempt copying of a1 into a2
     TEST_ASSERT(memcmp(a1, a2, sizeof a1) != 0);
-    printf("MEMCMP: %d", memcmp(a1, a2, sizeof a1));
     algorithm_copy(char*, a1, a1 + sz, a2);
 
     //Assert copied values
     TEST_ASSERT_EQUAL_CHAR_ARRAY(a1, a2, sz);
+}
+
+void test_algorithm_copy_struct() {
+    //Setting up basic array of structs to copy over
+    struct point a1[] = {{1, 2}, {4, 5}, {10, 10}};
+    const int sz = sizeof a1 / sizeof(struct point);
+    struct point a2[sz];
+
+    //Attempt copying of a1 into a2
+    TEST_ASSERT(memcmp(a1, a2, sizeof a1) != 0);
+    algorithm_copy(struct point*, a1, a1 + sz, a2);
+
+    //Assert copied values
     TEST_ASSERT(memcmp(a1, a2, sizeof a1) == 0);
-    printf("MEMCMP: %d", memcmp(a1, a2, sizeof a1));
 }
