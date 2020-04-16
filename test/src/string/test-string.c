4f615f93b5a7b9da7d96e865fe3a5f9e2c651bb4
Luiserebii <luis@serebii.io>
2020-04-16 01:31:08 -0400

    Fleshed out test for setting c-strings to strings

--- test/src/string/test-string.c
+++ test/src/string/test-string.c
@@ -25,3 +25,41 @@ void test_string_init() {
 
     string_free(s);
 }
+
+void test_string_set_cstr() {
+    string s = string_init();
+    
+    //Test setting a c-string to an empty string
+    const char* cstr = "Hello";
+    const size_t sz = 5;
+    string_set_cstr(s, cstr);
+
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(cstr, s->head, sz);
+    TEST_ASSERT_EQUAL_INT(sz, string_size(s));
+
+    //Test setting a larger c-string to a string with contents
+    const char* cstr2 = "I am a giraffe";
+    const size_t sz2 = 14;
+    string_set_cstr(s, cstr2);
+
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(cstr2, s->head, sz2);
+    TEST_ASSERT_EQUAL_INT(sz2, string_size(s));
+
+    //Test setting a smaller c-string to a string with contents
+    const char* cstr3 = "meme";
+    const size_t sz3 = 4;
+    string_set_cstr(s, cstr3);
+
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(cstr3, s->head, sz3);
+    TEST_ASSERT_EQUAL_INT(sz3, string_size(s));
+
+    string_free(s);
+}
+
+void test_string_cat() {
+
+}
+
+void test_string_cstr() {
+
+}
