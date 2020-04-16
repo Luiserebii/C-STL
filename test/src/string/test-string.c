50b506b0694088043729bd4118d463b3bc740a5f
Luiserebii <luis@serebii.io>
2020-04-16 01:39:27 -0400

    Adding tests for string_cat

--- test/src/string/test-string.c
+++ test/src/string/test-string.c
@@ -57,7 +57,30 @@ void test_string_set_cstr() {
 }
 
 void test_string_cat() {
+    string s = string_init();
 
+    //Test concatenating a string to an empty string
+    string str1 = string_init_cstr("Hello");
+    size_t sz1 = 5;
+
+    string_cat(s, str1);
+
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(str1->head, s->head, sz1);
+    TEST_ASSERT_EQUAL_INT(sz1, string_size(s));
+
+    //Test concatenating a string to a string with content
+    string str2 = string_init_cstr(" world");
+    size_t sz2 = 6;
+
+    string_cat(s, str2);
+   
+    const char[] exp = "Hello world";  
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(exp, s->head, sz1 + sz2);
+    TEST_ASSERT_EQUAL_INT(sz1 + sz2, string_size(s));
+
+    string_free(s);
+    string_free(str1);
+    string_free(str2);
 }
 
 void test_string_cstr() {
