0824a9ca0045400c36811e2f0f1b048a8d2c7190
Luiserebii <luis@serebii.io>
2020-05-01 02:56:42 -0400

    Minor attempt to try to make string cstr() const-correct; deciding to allocate one more for the null term behind the scenes, but required expanding everything, and needs a rewrite. (There is one way to potentially refactor this by giving vector a way to maybe use a macro for allocs that can be supplanted...)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find ./include -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
+find ./src -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
 find ./test/src -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
 find ./test/include -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
